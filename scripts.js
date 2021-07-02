daafe306621cf6f9b126ac1c5a3e23a167a8613d
yyosifov <cypressx@gmail.com>
2021-07-02 14:35:00 +0300

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,6 +1,11 @@
 // Compact all collections in a database
-
 db.getCollectionNames().forEach(function (collectionName) {
     print('Compacting: ' + collectionName);
     db.runCommand({ compact: collectionName });
 });
+
+// Get the size of all collections
+db.getCollectionNames().forEach(function(c) {
+   s = db[c].stats();
+   printjson(s);
+})
